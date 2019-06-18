21c50c44255d398237c51ed92a1c24260a9fd99d
Pedro Silva <pedro.silva@wirepas.com>
2019-06-18 14:30:19 +0300

    Docker images building procedure
    This commit simplifies the building of docker images by
    relying on a docker-comopse for each type of image.
    
    It is still possible to reduce the build to a single
    compose file + a shell script to set the correct
    tags and paths. However this is left for future
    consideration.
    
    Closes #15 and #26

--- container/docker-entrypoint.sh
+++ container/docker-entrypoint.sh
@@ -4,97 +4,60 @@
 set -e
 env | grep WM_
 
-TARGET="${1}"
+TARGET=${1}
 
-if [ "${TARGET}" == "sink" ] || [ "${TARGET}" == "transport" ]
-then
-    echo "Executing ${TARGET}"
-    if [ "${TARGET}" = 'transport' ]; then
-        echo "connecting to ${WM_SERVICES_HOST}"
+# Environment
+#EXEC_TRANSPORT="wm-gw"
+#EXEC_SINK=/usr/local/bin/sinkService
 
-        # certificate chain
-        if [ ! -z "${WM_SERVICES_CERTIFICATE_CHAIN}" ]
-        then
-            CMD_SERVICES_CERTIFICATE_CHAIN="-t ${WM_SERVICES_CERTIFICATE_CHAIN}"
-        else
-            CMD_SERVICES_CERTIFICATE_CHAIN=""
+WM_SINK_UART_PORT=${WM_SINK_UART_PORT:-"/dev/ttyACM0"}
+WM_SINK_UART_BITRATE=${WM_SINK_UART_BITRATE:-"125000"}
+WM_SINK_ID=${WM_SINK_ID:-"0"}
 
-        fi
+function generate_settings
+{
+    TEMPLATE=${1}
+    OUTPUT_PATH=${2}
 
-        # gateway id
-        if [ ! -z "${WM_SERVICES_GATEWAY_ID}" ]
-        then
-            CMD_SERVICES_GATEWAY_ID="-i ${WM_SERVICES_GATEWAY_ID}"
-        else
-            CMD_SERVICES_GATEWAY_ID=""
-        fi
-
-        # unsecure connection
-        if [ ! -z "${WM_SERVICES_ALLOW_UNSECURE}" ] && [ "${WM_SERVICES_ALLOW_UNSECURE}"  == "true" ]
-        then
-            CMD_SERVICES_ALLOW_UNSECURE="-ua"
-        else
-            CMD_SERVICES_ALLOW_UNSECURE=""
-        fi
-
-        # gateway model
-        if [ ! -z "${WM_SERVICES_GATEWAY_MODEL}" ]
-        then
-            CMD_SERVICES_GATEWAY_MODEL="-gm ${WM_SERVICES_GATEWAY_MODEL}"
-        else
-            CMD_SERVICES_GATEWAY_MODEL=""
-        fi
-
-        # gateway version
-        if [ ! -z "${WM_SERVICES_GATEWAY_VERSION}" ]
-        then
-            CMD_SERVICES_GATEWAY_VERSION="-gv ${WM_SERVICES_GATEWAY_VERSION}"
-        else
-            CMD_SERVICES_GATEWAY_VERSION=""
-        fi
-
-        # endpoints to filter at the gateway
-        if [ ! -z "${WM_SERVICES_GATEWAY_IGNORED_ENDPOINTS_FILTER}" ]
-        then
-            CMD_SERVICES_GATEWAY_IGNORED_ENDPOINTS_FILTER="-iepf ${WM_SERVICES_GATEWAY_IGNORED_ENDPOINTS_FILTER}"
-        else
-            CMD_SERVICES_GATEWAY_IGNORED_ENDPOINTS_FILTER=""
-        fi
-
-        # endpoints to whiten at the gateway
-        if [ ! -z "${WM_SERVICES_GATEWAY_WHITENED_ENDPOINTS_FILTER}" ]
-        then
-            CMD_SERVICES_GATEWAY_WHITENED_ENDPOINTS_FILTER="-wepf ${WM_SERVICES_GATEWAY_WHITENED_ENDPOINTS_FILTER}"
-        else
-            CMD_SERVICES_GATEWAY_WHITENED_ENDPOINTS_FILTER=""
-        fi
-
-        TARGET="${EXEC_TRANSPORT} \
-                 -s ${WM_SERVICES_HOST} \
-                 -p ${WM_SERVICES_MQTT_PORT} \
-                 -u ${WM_SERVICES_MQTT_USER} \
-                 -pw ${WM_SERVICES_MQTT_PASSWORD} \
-                 ${CMD_SERVICES_CERTIFICATE_CHAIN} \
-                 ${CMD_SERVICES_GATEWAY_ID} \
-                 ${CMD_SERVICES_ALLOW_UNSECURE} \
-                 ${CMD_SERVICES_GATEWAY_MODEL} \
-                 ${CMD_SERVICES_GATEWAY_VERSION} \
-                 ${CMD_SERVICES_GATEWAY_IGNORED_ENDPOINTS_FILTER} \
-                 ${CMD_SERVICES_GATEWAY_WHITENED_ENDPOINTS_FILTER}"
+    if [[ -f "${OUTPUT_PATH}" ]]
+    then
+        rm -f "${OUTPUT_PATH}" "${OUTPUT_PATH}.tmp"
     fi
 
-    if [ "${TARGET}" = 'sink' ]; then
-       TARGET="${EXEC_SINK} \
-                -p ${WM_SINK_UART_PORT}
+    ( echo "cat <<EOF >${OUTPUT_PATH}";
+      cat "${TEMPLATE}";
+      echo "EOF";
+    ) > "${OUTPUT_PATH}.tmp"
+    # shellcheck source=/dev/null
+    . "${OUTPUT_PATH}.tmp"
+    rm "${OUTPUT_PATH}.tmp"
+
+    sed -i "/NOTSET/d" "${OUTPUT_PATH}"
+}
+
+generate_settings "${TRANSPORT_SERVICE}/transport.template" "${TRANSPORT_SERVICE}/transport.yaml"
+
+if [[ "${TARGET}" == "sink" || "${TARGET}" == "transport" ]]
+then
+    if [[ "${TARGET}" == "transport" ]]
+    then
+        echo "connecting to ${WM_SERVICES_HOST}"
+        TARGET="wm-gw --settings ${TRANSPORT_SERVICE}/transport.yaml"
+    fi
+
+    if [[ "${TARGET}" == "sink" ]]
+    then
+       TARGET="sinkService \
+                -p ${WM_SINK_UART_PORT} \
                 -b ${WM_SINK_UART_BITRATE} \
                 -i ${WM_SINK_ID}"
     fi
 
-    echo "running ${TARGET}"
-    exec ${TARGET}
+    echo "Starting service: ${TARGET}"
+    exec "${TARGET}"
 else
-    echo "executing ${@}"
-    exec "${@}"
+    echo "Starting custom: ${*}"
+    exec "${*}"
 fi
 
 echo "exiting"
