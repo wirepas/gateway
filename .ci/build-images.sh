d1a933b685089b144fff955b981f41aa103e5af5
Pedro Silva <pedro.silva@wirepas.com>
2019-07-08 14:15:30 +0300

    Install repo on travis
    This commit assumes the repo dependency
    is met and installed on the developer's
    host.

--- .ci/build-images.sh
+++ .ci/build-images.sh
@@ -4,6 +4,7 @@
 set -e
 
 DOCKERFILE_PATH="./container"
+GIT_REPO_FOLDER="_repo"
 
 ##
 ## @brief      Changes to the corresponding target and builds the image
@@ -29,12 +30,17 @@ function _pull_dev_dependencies
     GIT_MANIFEST_FILE=${GIT_MANIFEST_FILE:-"gateway.xml"}
     GIT_MANIFEST_URL=${GIT_MANIFEST_URL:-"https://github.com/wirepas/manifest.git"}
 
-    curl https://storage.googleapis.com/git-repo-downloads/repo > /usr/local/bin/repo
-    chmod a+x /usr/local/bin/repo
-    repo init -u "${GIT_MANIFEST_URL}" \
+    rm -rf "${GIT_REPO_FOLDER}"
+    mkdir "${GIT_REPO_FOLDER}"
+    cd "${GIT_REPO_FOLDER}"
+    pipenv run --two repo init -u "${GIT_MANIFEST_URL}" \
                  -m "${GIT_MANIFEST_FILE}" \
                  --no-clone-bundle
-    repo sync
+    pipenv --rm
+    pipenv run --two repo sync
+
+    cd ..
+    cp -r "${GIT_REPO_FOLDER}/sink_service/" .
 }
 
 
---
+++ .ci/install-repo.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Wirepas Oy
+
+set -e
+
+curl https://storage.googleapis.com/git-repo-downloads/repo > /usr/local/bin/repo
+
+chmod a+x /usr/local/bin/repo
