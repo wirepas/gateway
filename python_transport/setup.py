86ed2aa6796f38c2210eea0b4a624013acd1feeb
Pedro Silva <pedro.silva@wirepas.com>
2019-07-09 17:46:11 +0300

    Package metadata
    This commit moves all the package metadata, such as author, license,
    description, among others into a dedicated file.
    
    This approach allows us to remove the necessity of protecting
    module and requirement imports found in __init__.py.

--- python_transport/setup.py
+++ python_transport/setup.py
@@ -13,19 +13,14 @@
 import os
 import re
 
-from wirepas_gateway import __title__
-from wirepas_gateway import __version__
-
 from setuptools import setup, find_packages, Extension
 
+here = os.path.abspath(os.path.dirname(__file__))
 readme_file = "README.md"
 license_file = "LICENSE"
 
 with open(readme_file) as f:
     long_description = f.read()
-
-with open(license_file) as f:
-    license = f.read()
 
 
 def get_list_files(root, flist=None):
@@ -40,8 +35,7 @@ def get_list_files(root, flist=None):
 
 def get_absolute_path(*args):
     """ Transform relative pathnames into absolute pathnames """
-    directory = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(directory, *args)
+    return os.path.join(here, *args)
 
 
 def get_requirements(*args):
@@ -57,23 +51,21 @@ def get_requirements(*args):
     return sorted(requirements)
 
 
+about = {}
+with open(get_absolute_path("./wirepas_gateway/__about__.py")) as f:
+    exec(f.read(), about)
+
 setup(
-    name=__title__,
-    version=__version__,
-    description="Wirepas gateway transport service",
+    name=about["__pkg_name__"],
+    version=about["__version__"],
+    description=about["__description__"],
     long_description=long_description,
-    author="Wirepas Oy",
-    author_email="opensource@wirepas.com",
-    url="https://github.com/wirepas/gateway",
-    license="Apache-2",
-    classifiers=[
-        "Development Status :: 5 - Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Topic :: Software Development :: Libraries",
-        "Programming Language :: Python :: 3",
-    ],
-    keywords="wirepas connectivity iot mesh",
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    url=about["__url__"],
+    license=about["__license__"],
+    classifiers=about["__classifiers__"],
+    keywords=about["__keywords__"],
     packages=find_packages(exclude=["contrib", "docs", "tests", "examples"]),
     install_requires=get_requirements("requirements.txt"),
     ext_modules=[
---
+++ python_transport/wirepas_gateway/__about__.py
@@ -0,0 +1,23 @@
+"""
+    .. Copyright:
+        Wirepas Oy licensed under Apache License, Version 2.0
+        See file LICENSE for full license details.
+"""
+
+__author__ = "Wirepas Ltd"
+__author_email__ = "opensource@wirepas.com"
+__classifiers__ = [
+    "Development Status :: 5 - Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Topic :: Software Development :: Libraries",
+    "Programming Language :: Python :: 3",
+]
+__copyright__ = "2019 Wirepas Ltd"
+__description__ = "Wirepas gateway transport service that connects the local dbus to a remote MQTT broker."
+__license__ = "Apache-2"
+__pkg_name__ = "wirepas_gateway"
+__title__ = "Wirepas Gateway Transport Service"
+__url__ = "https://github.com/wirepas/gateway"
+__version__ = "1.2.0rc1"
+__keywords__ = "wirepas connectivity iot mesh"
