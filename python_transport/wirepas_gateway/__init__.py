f21004ce564fa8bd0aaa07dede0c3fd4bb7de914
Pedro Figueiredo e Silva <PFigs@users.noreply.github.com>
2019-07-12 10:26:30 +0300

    Package metadata (#57)
    This commit moves all the package metadata, such as author, license,
    description, among others into a dedicated file.
    
    This approach allows us to remove the necessity of protecting
    module and requirement imports found in __init__.py.

--- python_transport/wirepas_gateway/__init__.py
+++ python_transport/wirepas_gateway/__init__.py
@@ -1,38 +1,28 @@
 """
-    Wirepas Gateway Client
-    =========================================
-
-    This module is property of Wirepas Oy and is meant for its user. Usage
-    implies knowledge and acceptance of the license agreement provided
-    through the separate channels.
-
-    The goal of this module is to support the exchange of data from a
-    Wirepas gateway to a Wirepas or user backend system.
+    Wirepas Gateway
+    ===============
 
     .. Style guidelines:
        http://google.github.io/styleguide/pyguide.html
-
-    .. Attributes defined from _PEP 484:
-       https://www.python.org/dev/peps/pep-0484/
 
     .. Copyright:
         Wirepas Oy licensed under Apache License, Version 2.0
         See file LICENSE for full license details.
 """
-try:
-    from . import dbus
-except ImportError:
-    pass
 
-try:
-    from . import protocol
-except ImportError:
-    pass
+from . import dbus
+from . import protocol
+from . import utils
 
-try:
-    from . import utils
-except ImportError:
-    pass
-
-__title__ = "wirepas_gateway"
-__version__ = "1.2.0rc1"
+from .__about__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __description__,
+    __license__,
+    __pkg_name__,
+    __title__,
+    __url__,
+    __version__,
+    __keywords__,
+)
