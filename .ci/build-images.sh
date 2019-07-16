a979f378e07497060772950311e18b9a902624e8
Pedro Figueiredo e Silva <PFigs@users.noreply.github.com>
2019-07-16 13:25:08 +0300

    Build optimizations for Docker (#60)
    This commit removes the need of keeping two Dockerfiles for
    different architectures. 
    
    Similarly as before, the Dockerfiles are kept in two folders, a stable 
    for builds based on https://github.com/wirepas/manifest and a dev folder
    for builds based on the current content of the repo (+ master of c-mesh-api). 
    A commons folder contains templates and the docker-entrypoint script.
    
    Release builds are now only triggered when a tag is given to CI system, which
    is meant to prepare the future push from CI to dockerhub.
    
    There is still a need in the future to accommodate other OSes inside
    the container folder, such as Alpine.
    
    * Update readme (#63) Closes #62
    * Update wirepas_messaging version Bump to rc3. See #38

--- .ci/build-images.sh
+++ .ci/build-images.sh
@@ -3,55 +3,77 @@
 
 set -e
 
-DOCKERFILE_PATH="./container"
-GIT_REPO_FOLDER="_repo"
-
-export LXGW_VERSION
+export VERSION
 export BUILD_DATE
 export IMAGE_NAME
 export REGISTRY_NAME
+export DOCKER_BASE
+export CROSS_BUILD_START_CMD
+export CROSS_BUILD_END_CMD
+export GIT_MANIFEST_FILE
+export GIT_MANIFEST_URL
+export GIT_MANIFEST_BRANCH
 
-LXGW_VERSION=$(< python_transport/wirepas_gateway/__init__.py awk '/__version__/{print $NF}'| tr -d '\"')
+VERSION=$(< python_transport/wirepas_gateway/__init__.py awk '/__version__/{print $NF}'| tr -d '\"')
 BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-REGISTRY_NAME="wirepas/"
+REGISTRY_NAME="wirepas"
+
+DOCKERFILE_PATH="./container"
+GIT_REPO_FOLDER="./.ci/_repo"
+BUILD_TAG=${TRAVIS_TAG:-}
 
 ##
 ## @brief      Changes to the corresponding target and builds the image
 ##
 function _build
 {
-    TARGET=${1:-"dev"}
-    CURRENT_PATH="$(pwd)"
+    _PATH=${1:-"${DOCKERFILE_PATH}/x86"}
+    _ARCH=${2:-"x86"}
+    _CACHE=${3:-}
 
-    if [[ ${TARGET} == "dev" ]]
+    # build based on architecture
+    IMAGE_NAME="${REGISTRY_NAME}/gateway-${_ARCH}:${BUILD_TAG}"
+
+    if [[ ${_ARCH} == "arm" ]]
     then
-        _pull_dev_dependencies
+        DOCKER_BASE=wirepas/base:1.1-raspbian
+        CROSS_BUILD_START_CMD=cross-build-start
+        CROSS_BUILD_END_CMD=cross-build-end
+    else
+        DOCKER_BASE=wirepas/base:1.1-ubuntu
+        CROSS_BUILD_START_CMD=:
+        CROSS_BUILD_END_CMD=:
     fi
 
-    cd "${DOCKERFILE_PATH}/${TARGET}"
-    IMAGE_NAME="${REGISTRY_NAME}/gateway-${TARGET}"
-    docker-compose build --no-cache
-    cd "${CURRENT_PATH}"
+    echo "building ${_PATH}: ${IMAGE_NAME} (from: ${DOCKER_BASE})"
+    #shellcheck disable=SC2086
+    docker-compose -f "${_PATH}" \
+                   build ${_CACHE} \
+                   --compress \
+                   --parallel \
+
 }
 
-##
-## @brief      Setup repo and syncs the repositories
-##
-function _pull_dev_dependencies
+
+function _fetch_dependencies
 {
-    GIT_MANIFEST_FILE=${GIT_MANIFEST_FILE:-"gateway.xml"}
-    GIT_MANIFEST_URL=${GIT_MANIFEST_URL:-"https://github.com/wirepas/manifest.git"}
+    # pull repository dependency
+    GIT_MANIFEST_FILE=gateway.xml
+    GIT_MANIFEST_URL=https://github.com/wirepas/manifest.git
+    GIT_MANIFEST_BRANCH=master
+    _ROOT_PATH=$(pwd)
 
     rm -rf "${GIT_REPO_FOLDER}"
     mkdir "${GIT_REPO_FOLDER}"
     cd "${GIT_REPO_FOLDER}"
-    pipenv run --two repo init -u "${GIT_MANIFEST_URL}" \
-                 -m "${GIT_MANIFEST_FILE}" \
-                 --no-clone-bundle
+    pipenv run --two repo init \
+               -u "${GIT_MANIFEST_URL}" \
+               -m "${GIT_MANIFEST_FILE}" \
+               -b "${GIT_MANIFEST_BRANCH}" \
+               --no-clone-bundle
     pipenv --rm
     pipenv run --two repo sync
-
-    cd ..
+    cd "${_ROOT_PATH}"
     cp -r "${GIT_REPO_FOLDER}/sink_service/" .
 }
 
@@ -61,9 +83,21 @@ function _pull_dev_dependencies
 ##
 function _main
 {
-    _build "arm"
-    _build "x86"
-    _build "dev"
+
+    # builds x86 and arm images based on manifest files
+    if [[ ! -z ${BUILD_TAG} ]]
+    then
+        GIT_MANIFEST_BRANCH=refs/tags/${BUILD_TAG}
+        _build "${DOCKERFILE_PATH}/arm/docker-compose.yml" "arm" "--no-cache"
+        _build "${DOCKERFILE_PATH}/x86/docker-compose.yml" "x86" "--no-cache"
+    else
+        BUILD_TAG="edge"
+    fi
+
+    # builds x86 and arm images based on top of current revision
+    _fetch_dependencies
+    _build "${DOCKERFILE_PATH}/dev/docker-compose.yml" "arm"
+    _build "${DOCKERFILE_PATH}/dev/docker-compose.yml" "x86"
 }
 
 
