68f3ceed0955eafab369cfdedd0b502d74f2f07d
Pedro Silva <pedro.silva@wirepas.com>
2019-07-16 11:19:13 +0300

    Validate if wheel is valid for distribution

--- .ci/build.sh
+++ .ci/build.sh
@@ -7,4 +7,5 @@ TARGET_DIR=${TARGET_DIR:-"python_transport"}
 cd "${TARGET_DIR}"
 
 ./utils/generate_wheel.sh
-./utils/generate_sphinx_docs.sh || true
+./utils/generate_sphinx_docs.sh
+twine check dist/*
