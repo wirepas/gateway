069c70971deaceb244114668ca75f37eb552dc2d
Pedro Figueiredo e Silva <pedro.silva@wirepas.com>
2019-07-20 10:38:05 +0300

    Deploy to dockerhub from travis

---
+++ .ci/deploy-docker.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+# Wirepas Oy
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+DOCKER_TAG=${TRAVIS_TAG:-"edge"}
+IS_RELEASE=${TRAVIS_TAG:-"false"}
+
+DOCKER_USERNAME=${DOCKER_USERNAME}
+DOCKER_PASSWORD=${DOCKER_PASSWORD}
+DOCKER_ORG=${DOCKER_ORG:-"wirepas"}
+DOCKER_IMAGE=${DOCKER_IMAGE:-"gateway"}
+
+function _push
+{
+    _TAG=${1}
+
+    # push images and manifest
+    docker push wirepas/gateway-x86:"${_TAG}"
+    docker push wirepas/gateway-arm:"${_TAG}"
+    docker push wirepas/gateway:"${_TAG}"
+}
+
+function _create_manifest()
+{
+    _TAG=${1}
+
+    # creates the manifest for x86 and ARM
+    docker manifest create wirepas/gateway:"${_TAG}" \
+                           wirepas/gateway-x86:"${_TAG}" \
+                           wirepas/gateway-arm:"${_TAG}"
+
+    # sets the correct platform for rpi
+    docker manifest annotate wirepas/gateway:"${_TAG}" \
+                             wirepas/gateway-arm:"${_TAG}" \
+                             --arch arm
+}
+
+
+function _main()
+{
+    echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+
+    _create_manifest "${DOCKER_TAG}"
+    _push "${DOCKER_TAG}"
+
+    if [[ "${IS_RELEASE}" != "false" ]]
+    then
+        _create_manifest "latest"
+        _push "latest"
+    fi
+}
+
+_main "$@"
