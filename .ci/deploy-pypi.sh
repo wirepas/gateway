2fac7a841f45b5f80ae7c6ddaa1cd42a5446b4b4
Pedro Figueiredo e Silva <PFigs@users.noreply.github.com>
2019-07-22 11:37:42 +0300

    Disable linux wheel upload to pypi (#90)
    This commit change the deploy-pypi function to disallow uploads of linux wheels as they are not allowed on PyPi.

--- .ci/deploy-pypi.sh
+++ .ci/deploy-pypi.sh
@@ -1,12 +1,49 @@
 #!/usr/bin/env bash
-# Wirepas Oy
+# Copyright Wirepas Ltd 2019
 
-ARTIFACT_PATH=${1:-"./dist/*"}
+set -e
+
+ARTIFACT_PATH=( "${@}" )
 
 TWINE_USERNAME=${TWINE_USERNAME:-"Wirepas"}
 TWINE_PASSWORD=${TWINE_PASSWORD:-${PYPI_PWD}}
 TWINE_REPOSITORY=${TWINE_REPOSITORY}
 TWINE_REPOSITORY_URL=${TWINE_REPOSITORY_URL}
 
-twine check "${ARTIFACT_PATH}"
-twine upload "${ARTIFACT_PATH}"
+function pypi_validate_package()
+{
+    for _file in "${ARTIFACT_PATH[@]}"
+    do
+        twine check "${_file}"
+    done
+}
+
+
+function pypi_upload()
+{
+
+    for _file in "${ARTIFACT_PATH[@]}"
+    do
+
+        if [[ "${_file}" == *"linux_x86_64.whl" ]]
+        then
+            echo "linux platform wheel not allowed on PyPi. Skipping upload"
+            continue
+        fi
+
+        echo "uploading file ${_file} --> ${TWINE_REPOSITORY_URL}"
+        twine upload "${_file}" --verbose --skip-existing
+
+    done
+}
+
+
+function _main()
+{
+
+    pypi_validate_package
+    pypi_upload
+
+}
+
+_main
