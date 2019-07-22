fb72aa4267d6841011b31b24d506a8d64b16970f
Pedro Silva <pedro.silva@wirepas.com>
2019-07-22 08:39:32 +0300

    Update license header
    Cahnges the license header to add year and wirepas' international
    name (ltd instead of oy).

--- python_transport/wirepas_gateway/__main__.py
+++ python_transport/wirepas_gateway/__main__.py
@@ -5,7 +5,7 @@
     Package main file
 
     .. Copyright:
-        Wirepas Oy licensed under Apache License, Version 2.0
+        Copyright 2019 Wirepas Ltd licensed under Apache License, Version 2.0
         See file LICENSE for full license details.
 """
 
