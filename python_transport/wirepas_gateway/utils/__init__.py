1b42b38e96278ad82bcbef9ffeacd12d7376ea3a
Gwendal Raoul <gwendal.raoul@wirepas.com>
2019-07-26 17:05:02 +0200

    Add flake8 ignore on __init__.py

--- python_transport/wirepas_gateway/utils/__init__.py
+++ python_transport/wirepas_gateway/utils/__init__.py
@@ -1,6 +1,7 @@
 # Copyright 2019 Wirepas Ltd licensed under Apache License, Version 2.0
 #
 # See file LICENSE for full license details.
+# flake8: noqa
 
 from .log_tools import *
 from .serialization_tools import *
