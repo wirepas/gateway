d10e572f66075ea7934ce0ce093fcdcff9ea71c0
Pedro Silva <pedro.figs.silva@gmail.com>
2019-11-24 18:54:33 +0200

    Remove template generattion from ci test

--- .ci/test-template-generation.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# shellcheck disable=SC1091
-source container/common/generate_settings.sh
-
-generate_settings container/common/wm_transport_service.template container/common/wm_transport_service.yml
