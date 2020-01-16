b3ba1f66c639ac91c6bfdf4a4a472724901ce3e7
Pedro Silva <pedro.silva@wirepas.com>
2020-01-16 16:49:32 +0200

    Handle '' as False

--- python_transport/wirepas_gateway/utils/argument_tools.py
+++ python_transport/wirepas_gateway/utils/argument_tools.py
@@ -161,7 +161,7 @@ class ParserHelper:
             return value
         if value.lower() in ("yes", "true", "t", "y", "1"):
             return True
-        elif value.lower() in ("no", "false", "f", "n", "0"):
+        elif value.lower() in ("no", "false", "f", "n", "0", ""):
             return False
         else:
             raise argparse.ArgumentTypeError("Boolean value expected.")
