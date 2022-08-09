ba3b257233580f1fa866aea3d486415928bd47fe
Gwendal Raoul <gwendal.raoul@wirepas.com>
2022-08-09 14:18:38 +0200

    Cleanup of code and handle ipv6 packets addressed to interface

--- ipv6_service/ipv6_service.py
+++ ipv6_service/ipv6_service.py
@@ -4,7 +4,9 @@ import fcntl
 import os
 import struct
 import subprocess
-import sys
+import socket
+import select
+import logging
 
 from datetime import datetime
 from threading import Thread
@@ -149,9 +151,176 @@ class IPV6Transport(BusClient):
     IPV6 transport:
     """
 
+    class IPV6Sink(Thread):
+
+        UDP_INTERFACE_PORT = 666
+
+        """Inner class to represent a sink at ipv6 level"""
+        def __init__(self, sink, nw_prefix, ext_interface_name):
+
+            Thread.__init__(self)
+            # Daemonize thread to exit with full process
+            self.daemon = True
+            self.running = False
+
+            self._sink = sink
+
+            self.nw_prefix = nw_prefix
+
+            self._ext_interface = ext_interface_name
+
+            sink_config = self._sink.read_config()
+            if not sink_config["started"]:
+                # Do not add sink that are not started, will be done later
+                logging.warning("Sink not started, do not add it yet")
+                raise RuntimeError("Stack not started yet")
+
+            # Create a set to store already added neighbor proxy entry
+            # to avoid too many call to "ip"
+            self._neigh_proxy = set()
+
+            self._wp_address = sink_config["node_address"]
+
+            # Network address not needed at the moment
+            #self.network_add = sink.get_network_address()
+
+            self._add_prefix_to_app_config(sink_config)
+
+            self._ipv6_interface_address = Ipv6Add.from_prefix_sink_add_and_sink_node(self.nw_prefix, self.wp_address, 0)
+
+            self._ipv6_sink_prefix = Ipv6Add.from_prefix_and_sink_add(self.nw_prefix, self.wp_address)
+
+            # Create socket pair to wakeup the thread waiting on data
+            self._sp_w, self._sp_r = socket.socketpair()
+
+
+        @property
+        def wp_address(self):
+            return self._wp_address
+
+        @property
+        def ipv6_interface_address(self):
+            return self._ipv6_interface_address
+
+        @property
+        def ipv6_sink_prefix(self):
+            return self._ipv6_sink_prefix
+
+        def send_data(self, node_address, data):
+            self._sink.send_data(
+                        node_address,
+                        WIREPAS_IPV6_EP,
+                        WIREPAS_IPV6_EP,
+                        1,
+                        0,
+                        data,
+                        False,
+                        0)
+
+        def run(self):
+            # Open UDP socket
+            sock = socket.socket(socket.AF_INET6,
+                            socket.SOCK_DGRAM)
+
+            # Set socket to non blocking as we are using select
+            sock.setblocking(0)
+
+            # Bind it to ourself
+            sock.bind(("%s" % self._ipv6_interface_address, self.UDP_INTERFACE_PORT))
+
+            logging.debug("Waiting for packet")
+            self.running = True
+            while self.running:
+                try:
+                    r, _, _ = select.select([sock, self._sp_r], [], [])
+                except socket.error:
+                    continue
+
+                if self._sp_r in r:
+                    # We were wakeuped from other thread, iterate again to check running state
+                    _ = self._sp_r.recv(1)
+                    continue
+
+                if sock in r:
+                    try:
+                        data, addr = sock.recvfrom(2048)
+                        self.add_ndp_entry(Ipv6Add.from_srting(addr[0]).wirepas_node_add)
+                        logging.debug(data)
+                    except socket.error:
+                        logging.error("Cannot read socket even if select said that it was ready")
+
+            sock.close()
+            logging.info("Thread exited")
+
+
+        def stop(self):
+            """
+            Stoping the thread witing on own socket
+            """
+            self.running = False
+
+            logging.info("Stopping Thread")
+
+            # Waking up the thread w
+            self._sp_w.send(b"x")
+
+            self.join()
+
+            # make it a list to avoid size change while iteration
+            for neigh in list(self._neigh_proxy):
+                self.remove_ndp_entry(neigh)
+
+        def add_ndp_entry(self, node_address):
+            if node_address in self._neigh_proxy:
+                # Already in neighbor proxy cache
+                return
+
+            add = Ipv6Add.from_prefix_sink_add_and_sink_node(self.nw_prefix, self.wp_address, node_address)
+            IPV6Transport._execute_cmd("sudo ip neigh add proxy %s dev %s extern_learn" % (add, self._ext_interface),
+                                        True)
+
+            self._neigh_proxy.add(node_address)
+
+
+        def remove_ndp_entry(self, node_address):
+            if node_address not in self._neigh_proxy:
+                logging.error("Cannot remove proxy neighbor that was not previously added %x" % node_address)
+                return
+
+            add = Ipv6Add.from_prefix_sink_add_and_sink_node(self.nw_prefix, self.wp_address, node_address)
+            IPV6Transport._execute_cmd("sudo ip neigh del proxy %s dev %s" % (add, self._ext_interface),
+                                        True)
+
+            self._neigh_proxy.discard(node_address)
+
+
+        def _add_prefix_to_app_config(self, sink_config):
+            new_config = None
+            # Add network prefix to app_config tlv with id 66
+            try:
+                current_app_config = sink_config["app_config_data"]
+                app_config = WirepasTLVAppConfig.from_value(current_app_config)
+            except KeyError:
+                app_config = WirepasTLVAppConfig()
+            except ValueError:
+                #  Not tlv format, errase it
+                logging.info("Current app config is not with TLV format, erase it!")
+                app_config = WirepasTLVAppConfig()
+
+            app_config.add_entry(66, self.nw_prefix.prefix)
+
+            new_config = {}
+            new_config["app_config_data"] = app_config.value
+            new_config["app_config_seq"] = 0
+            new_config["app_config_diag"] = sink_config["app_config_diag"]
+
+            logging.info("Setting new app config with network prefix %s", self.nw_prefix)
+            self._sink.write_config(new_config)
+
+
     def __init__(self, external_interface="tap0", wp_interface="tun_wirepas") -> None:
 
-        # Initialize local varaible
+        # Initialize local variable
         self.ext_interface = external_interface
         self.wp_interface = wp_interface
 
@@ -169,7 +338,7 @@ class IPV6Transport(BusClient):
         # Get the network prefix associated with the external interface
         self.nw_prefix = self._get_external_prefix()
 
-        print("Network prefix is: %s " % self.nw_prefix)
+        logging.info("Network prefix is: %s " % self.nw_prefix)
 
         # Add a default route for network to tap interface
         IPV6Transport._execute_cmd("sudo ip -6 route add %s dev %s" % (self.nw_prefix, self.ext_interface),
@@ -186,62 +355,43 @@ class IPV6Transport(BusClient):
 
 
     def _add_sink_entry(self, name):
+        # Get sink object based on its name (sink0, sink1,...)
         sink = self.sink_manager.get_sink(name)
-        sink_config = sink.read_config()
-        if not sink_config["started"]:
-            # Do not add sink that are not started, will be done later
-            print("Sink not started, do not add it")
+
+        try:
+            ipv6_sink = IPV6Transport.IPV6Sink(sink, self.nw_prefix, self.ext_interface)
+        except RuntimeError:
+            # Not an issue, it will be added when stack is started
             return
 
-        sink_add = sink_config["node_address"]
-        network_add = sink.get_network_address()
-
-        # Create a set to store already added neighbor proxy entry
-        # to avoid too many call to "ip"
-        neigh_proxy = set()
+        self.sink_dic[name] = ipv6_sink
 
         # Add a route for this sink
-        self.add_route_to_tun_interface(sink_add)
+        self._add_route_to_tun_interface(ipv6_sink.ipv6_sink_prefix)
 
-        # Add network prefix to app_config tlv with id 66
-        try:
-            current_app_config = sink_config["app_config_data"]
-            app_config = WirepasTLVAppConfig.from_value(current_app_config)
-        except KeyError:
-            app_config = WirepasTLVAppConfig()
-        except ValueError:
-            #  Not tlv format, errase it
-            print("Current app config is not with TLV format, erase it!")
-            app_config = WirepasTLVAppConfig()
+        self._add_address_to_tun_interface(ipv6_sink.ipv6_interface_address)
 
-        app_config.add_entry(66, self.nw_prefix.prefix)
-
-        new_config = {}
-        new_config["app_config_data"] = app_config.value
-        new_config["app_config_seq"] = 0
-        new_config["app_config_diag"] = sink_config["app_config_diag"]
-
-        sink.write_config(new_config)
-
-        self.sink_dic[name] = (sink_add, network_add, neigh_proxy)
+        ipv6_sink.start()
 
     def _remove_sink_entry(self, name):
         try:
-            sink_add, network_add, neigh_proxy_set = self.sink_dic[name]
-            self.remove_route_to_tun_interface(sink_add)
+            ipv6_sink = self.sink_dic[name]
 
-            # make it a list to avoid size change while iteration
-            for neigh in list(neigh_proxy_set):
-                self.remove_ndp_entry(name, neigh)
+            self._remove_route_to_tun_interface(ipv6_sink.ipv6_sink_prefix)
+
+            self._remove_address_from_tun_interface(ipv6_sink.ipv6_interface_address)
+
+            ipv6_sink.stop()
 
             # Remove it from our dic
             del self.sink_dic[name]
 
         except KeyError:
-            print("Sink %s was not in our list" % name)
+            logging.error("Sink %s was not in our list, cannot remove it" % name)
 
     # Inherited methods from BusClient
     def on_sink_connected(self, name):
+        # Will be added only if sink is started
         self._add_sink_entry(name)
 
     def on_sink_disconnected(self, name):
@@ -255,7 +405,6 @@ class IPV6Transport(BusClient):
         # When stack is stopped, do not route traffic to us and consider
         # the sink as being removed
         self._remove_sink_entry(name)
-
 
     def on_data_received(
         self,
@@ -271,28 +420,25 @@ class IPV6Transport(BusClient):
         data,
     ):
         if src_ep == WIREPAS_IPV6_EP and dst_ep == WIREPAS_IPV6_EP:
-            print(
-                "[%s] Sink %s FROM %d TO %d on EP %d Data Size is %d" % (
-                datetime.utcfromtimestamp(int(timestamp / 1000)).strftime(
-                    "%Y-%m-%d %H:%M:%S"
-                ),
+            logging.info(
+                "Ipv6 traffic from wp nw on sink %s FROM %d TO %d Data Size is %d" % (
                 sink_id,
                 src,
                 dst,
-                dst_ep,
                 len(data))
             )
+            #logging.info(data.hex(" ", 2))
 
             # Inject it as is to tun interface
             os.write(self.tun.fileno(), data)
-        else:
-            # Update ndproxy based on traffic
-            # Maybe should be done only for ipv6 traffic, to not add non ipv6 node
-            self.add_ndp_entry(sink_id, src)
+
+        # Update ndproxy based on traffic
+        # Maybe should be done only for ipv6 traffic, to not add non ipv6 node
+        # self._add_ndp_entry(sink_id, src)
 
     @classmethod
     def _execute_cmd(cls, cmd, raise_exception=False):
-        print("executing cmd: %s" % cmd)
+        logging.info("executing cmd: %s" % cmd)
         result = subprocess.run(
             ["%s" % cmd],
             capture_output=True,
@@ -307,70 +453,59 @@ class IPV6Transport(BusClient):
     def _get_external_prefix(self):
         # For now only get the first prefix from the given interface and consider
         # it as our network prefix
+        # TODO add retry waiting for tap0 to be created by VPN and also for route to be propagated
         out = IPV6Transport._execute_cmd(
             "sudo rdisc6 -q -1 %s" % self.ext_interface, True)
 
         return Ipv6Add.from_srting(out)
 
     def _remove_tun_interface(self):
-        print("Remove tun interface " + self.wp_interface)
+        logging.info("Remove tun interface " + self.wp_interface)
         IPV6Transport._execute_cmd(
             "sudo ip tuntap del mode tun dev %s" % self.wp_interface)
 
     def _create_tun_interface(self):
-        print("Create tun interface")
+        logging.info("Create tun interface")
         IPV6Transport._execute_cmd(
             "sudo ip tuntap add mode tun dev %s user wirepas" % self.wp_interface,
             True)
 
-        print("Bring it up")
+        logging.info("Bring tun interface up")
         IPV6Transport._execute_cmd(
             "sudo ip link set %s up" % self.wp_interface,
             True)
 
-    def add_route_to_tun_interface(self, sink_id):
-        prefix = Ipv6Add.from_prefix_and_sink_add(self.nw_prefix, sink_id)
-
-        IPV6Transport._execute_cmd("sudo ip -6 route add %s dev %s metric 1" % (prefix, self.wp_interface),
+    def _add_route_to_tun_interface(self, sink_prefix):
+        IPV6Transport._execute_cmd("sudo ip -6 route add %s dev %s metric 1" % (sink_prefix, self.wp_interface),
                                     True)
 
-    def remove_route_to_tun_interface(self, sink_id):
-        prefix = Ipv6Add.from_prefix_and_sink_add(self.nw_prefix, sink_id)
-
-        IPV6Transport._execute_cmd("sudo ip -6 route del %s dev %s" % (prefix, self.wp_interface),
+    def _remove_route_to_tun_interface(self, sink_prefix):
+        IPV6Transport._execute_cmd("sudo ip -6 route del %s dev %s" % (sink_prefix, self.wp_interface),
                                     True)
 
-    def add_ndp_entry(self, sink_id, node_address):
+    def _add_address_to_tun_interface(self, address):
+        IPV6Transport._execute_cmd(
+            "sudo ip address add %s dev %s" % (address, self.wp_interface),
+            True)
+
+    def _remove_address_from_tun_interface(self, address):
+        IPV6Transport._execute_cmd(
+            "sudo ip address del %s dev %s" % (address, self.wp_interface),
+            True)
+
+    def _add_ndp_entry(self, sink_id, node_address):
         try:
-            sink_add, network_add, neigh_proxy_set = self.sink_dic[sink_id]
-            # Check our own cache to avoid adding it always
-            if node_address in neigh_proxy_set:
-                # Node already in neigh_proxy cache
-                return
-
-            add = Ipv6Add.from_prefix_sink_add_and_sink_node(self.nw_prefix, sink_add, node_address)
-            IPV6Transport._execute_cmd("sudo ip neigh add proxy %s dev %s extern_learn" % (add, self.ext_interface),
-                                        True)
-
-            neigh_proxy_set.add(node_address)
+            ipv6_sink = self.sink_dic[sink_id]
+            ipv6_sink.add_ndp_entry(node_address)
         except KeyError:
-            print("Sink %s was not in our list" % sink_id)
+            logging.error("Sink %s was not in our list, cannot add ndp entry" % sink_id)
 
-    def remove_ndp_entry(self, sink_id, node_address):
+    def _remove_ndp_entry(self, sink_id, node_address):
         try:
-            sink_add, network_add, neigh_proxy_set = self.sink_dic[sink_id]
-            # Check our own cache to avoid adding it always
-            if node_address not in neigh_proxy_set:
-                print("Cannot remove proxy neighbor that was not previously added")
-                return
-
-            add = Ipv6Add.from_prefix_sink_add_and_sink_node(self.nw_prefix, sink_add, node_address)
-            IPV6Transport._execute_cmd("sudo ip neigh del proxy %s dev %s" % (add, self.ext_interface),
-                                        True)
-
-            neigh_proxy_set.discard(node_address)
+            ipv6_sink = self.sink_dic[sink_id]
+            ipv6_sink.remove_ndp_entry(node_address)
         except KeyError:
-            print("Sink %s was not in our list" % sink_id)
+            logging.error("Sink %s was not in our list, cannot remove ndp entry" % sink_id)
 
     def _get_tun_interface_fd(self):
         # Some constants used to ioctl the device file
@@ -387,12 +522,13 @@ class IPV6Transport(BusClient):
 
         return tun
 
+
     def start(self):
 
         self.busThread.start()
 
         while True:
-            # Read an IP packet been sent to this TUN device.
+            # Wait for packet on TUN device.
             packet = bytearray(os.read(self.tun.fileno(), 2048))
 
             # Only propagate icmpv6 (58) and UDP traffic (17)
@@ -400,54 +536,39 @@ class IPV6Transport(BusClient):
             if next_header != 58 and next_header != 17:
                 continue
 
-            #payload_length = struct.unpack(">H", packet[4:6])[0]
-
+            # Todo, only do it under debug
             try:
                 src_addr = Ipv6Add(packet[8:24])
                 dst_addr = Ipv6Add(packet[24:40])
             except ValueError:
-                print("Cannot parse ipv6 address")
+                logging.error("Cannot parse ipv6 address")
 
-            print("ICMPV6: " + str(src_addr) + " => " + str(dst_addr))
+            logging.info("[%s]: %s => %s" % (next_header, src_addr, dst_addr))
+            logging.debug("Sink: 0x%x Node: 0x%x" % (dst_addr.wirepas_sink_add, dst_addr.wirepas_node_add))
 
-            print("Sink: 0x%x" % dst_addr.wirepas_sink_add)
-            print("Node: 0x%x" % dst_addr.wirepas_node_add)
 
             # Check if destination address is for our network
             # if not dst_addr.start_with_prefix(self.network_prefix.add[0:8]):
             #    print("Not for us")
             #    continue
 
-            for sink_id, sink_config in self.sink_dic.items():
-                sink_add, network_add, neigh_proxy = sink_config
-                if sink_add == dst_addr.wirepas_sink_add:
-                    print("sink found")
-                    self._send_data(
-                        sink_id,
+            # Send IPV6 packet from correct sink
+            sent = False
+            for ipv6_sink in self.sink_dic.values():
+                if ipv6_sink.wp_address == dst_addr.wirepas_sink_add:
+                    ipv6_sink.send_data(
                         dst_addr.wirepas_node_add,
                         bytes(packet))
+                    sent = True
                     break
 
-    def _send_data(self, sink_id, dest_add, payload):
-        sink = self.sink_manager.get_sink(sink_id)
-        if sink is not None:
-            res = sink.send_data(
-                dest_add,
-                WIREPAS_IPV6_EP,
-                WIREPAS_IPV6_EP,
-                1,
-                0,
-                payload,
-                False,
-                0)
-        else:
-            self.logger.error("No sink with id: %s", sink_id)
-
-
+            if not sent:
+                logging.error("No sink with id: %s", dst_addr.wirepas_sink_add)
 
 
 def main():
     
+    logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s', level=logging.INFO)
     ipv6_transport = IPV6Transport()
     # Start transport
     ipv6_transport.start()
