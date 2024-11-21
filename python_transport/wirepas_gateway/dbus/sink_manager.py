5139059fea42e1ed360e8f9ea80f9460fa90d06e
Jan Kowalewski <jkowalewski@cthings.co>
2024-11-21 16:37:25 +0100

    Add scanning neighbors feature
    This commit adds neighbors scanning feature and enables
    getting neighbors information over DBus from sink_service.
    It's a missing feature for network discovery and analysis.
    
    Signed-off-by: Jan Kowalewski <jkowalewski@cthings.co>

--- python_transport/wirepas_gateway/dbus/sink_manager.py
+++ python_transport/wirepas_gateway/dbus/sink_manager.py
@@ -137,6 +137,32 @@ class Sink:
         dic[key1] = att1_val
         dic[key2] = att2_val
 
+    def start_scan(self):
+        config = {}
+        config["sink_id"] = self.sink_id
+
+        # Should always be available
+        try:
+            config["started"] = (self.proxy.StackStatus & 0x01) == 0
+        except GLib.Error as e:
+            error = ReturnCode.error_from_dbus_exception(str(e))
+            logging.error("Cannot get Stack state: %s", error.name)
+
+        return self.proxy.StartScan()
+
+    def get_nbors(self):
+        config = {}
+        config["sink_id"] = self.sink_id
+
+        # Should always be available
+        try:
+            config["started"] = (self.proxy.StackStatus & 0x01) == 0
+        except GLib.Error as e:
+            error = ReturnCode.error_from_dbus_exception(str(e))
+            logging.error("Cannot get Stack state: %s", error.name)
+
+        return self.proxy.GetNbors
+
     def read_config(self):
         config = {}
         config["sink_id"] = self.sink_id
